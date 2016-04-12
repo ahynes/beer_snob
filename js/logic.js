3d35920a3ad2eb1bdad0de877ceafa7ea2d22670
ahynes <aoife_hynes@hotmail.com>
2016-04-11 22:09:48 -0400

    Returning the beer names from products to HTML

--- js/logic.js
+++ js/logic.js
@@ -3,6 +3,20 @@ $(document).ready(function() {
   		
 });
 
+var beerNames = [];
 function getBeers() {
+	$.ajax({
+		type: 'GET',
+	  	url: 'https://lcboapi.com/products',
+	  	headers: { 'Authorization': 'Token MDo4ZWY2ZTE4ZS0wMDNiLTExZTYtOGViYi1mMzJlYjk0NzBkYTA6RGdpaUFBVk1Bd050Q0dwVEFzTlF4S2JDT3BMNEY5YUIyWlQy' },
+	  	success: function(data){
+	  		console.log(data);
+	  		// document.getElementById('results').innerHTML = data;
+		  		$.each(data.result, function() {        
+			    beerNames.push(this.name);
+			    document.getElementById('results').innerHTML = beerNames;
+			});
+	  	}
+	});
 	alert("BEER BEER BEER!")
 }
